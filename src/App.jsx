10bae469736714facb9a19cfe645ad0e78022430
Erick <erickgp51@gmail.com>
2024-01-16 16:19:07 -0600

    terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import Header from './components/header';
+import Header from './components/Header'
 import Filtros from './components/Filtros';
 import ListadoGastos from './components/ListadoGastos';
 import Modal from './components/Modal';
